c1bed269cc12fd4debdf2a6e7e64514e40fdb0a6
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2021-09-20 14:55:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const downloadRoutes = require('./routes/download.routes');
-const procedimientosRoutes = require('./routes/procedimientos.routes')
 const app= express();
 let port = process.env.PORT || 3000;
 
