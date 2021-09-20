6bb9a851659883fe51e23faf9f2c3cdd5d367109
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2021-09-20 14:32:51 -0600

    Revert "Actualización Rutas JSON"
    This reverts commit f02b1ab97ba19f2a5f3c9ac65d5fc30d06ebf4fd.

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const downloadRoutes = require('./routes/download.routes');
-const procedimientosRoutes = require('./routes/procedimientos.routes')
 const app= express();
 let port = process.env.PORT || 3000;
 
@@ -8,8 +6,6 @@ app.get('/', (req, res)=>{
     res.send('Hello There!');
 });
 
-app.use(downloadRoutes);
-app.use(procedimientosRoutes);
 app.listen(port, ()=>{
     console.log(`Server on port ${port}`);
 });
