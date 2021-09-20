132f910928a0d77fe2d9750e2225e042d5977277
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2021-09-20 15:33:56 -0600

    Eliminar proyecto

--- routes/downloads.routes.js
+++
@@ -1,7 +0,0 @@
-const { getJSONData } = require('../controllers/download.controllers');
-
-const router = require('express').Router();
-
-router.get('/descargas/json/:id', getJSONData);
-
-module.exports=router;
--- routes/procedimientos.routes.js
+++
@@ -1,7 +0,0 @@
-const {getTipoProcedimientos} = require('../controllers/procedimientos.controllers')
-
-const router = require('express').Router();
-
-router.get('/procedimientos', getTipoProcedimientos);
-
-module.exports=router;
