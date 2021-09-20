a2dd41c4f2e871f0aab3b0c757c12a5cd0fcb584
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2021-09-20 10:36:33 -0600

    Eliminación Servidor
    Eliminación Servidor

--- server/src/config/config.js
+++
@@ -1,6 +0,0 @@
-const {config} = require('dotenv');
-config();
-console.log(process.env.PORT);
-module.exports={
-    port: process.env.PORT||3000
-};
--- server/src/routes/procedimientos.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const {getTipoProcedimiento} = require('../controllers/procedimientos.controllers');
-
-//Routes
-router.get('/procedimientos',getTipoProcedimiento);
-
-module.exports=router;
