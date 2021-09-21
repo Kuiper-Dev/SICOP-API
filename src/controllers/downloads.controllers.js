85197bcd063da0053c7a26e9a8028d449310f7ab
Alfredo Marrero <bernardo.marreroviquez@gmail.com>
2021-09-20 19:54:33 -0600

    Descarga XML
    Descarga XML

--- src/controllers/downloads.controllers.js
+++ src/controllers/downloads.controllers.js
@@ -5,7 +5,19 @@ downloadCtrl.getJSONData = async (req, res) =>{
         if(err){
             console.log(err);
         }else{
-            console.log('Yean Nice');
+            console.log('Descaragando');
+        }
+    });
+    console.log(__dirname);
+};
+
+downloadCtrl.getXMLData = async (req, res) =>{
+    
+    res.download('src/data/xml/'+req.params.id,req.params.id, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            console.log('Descargando');
         }
     });
     console.log(__dirname);
--- src/routes/downloads.routes.js
+++ src/routes/downloads.routes.js
@@ -3,3 +3,4 @@ const downloadCtrl = require('../controllers/downloads.controllers')
 module.exports=router;
 
 router.get('/descargas/json/:id', downloadCtrl.getJSONData);
+router.get('/descargas/xml/:id', downloadCtrl.getXMLData);
